24f2c60a772969c1a5a0fa10097b8a8e7f25c68e
wangxiang <261676607@qq.com>
2017-08-01 22:43:57 +0800

    修改border-1px出现的BUG，同时写好菜单栏

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@
       width: 100%
       height: 40px
       line-height: 40px
-      border-bottom-1px(rgba(7, 17, 27, 0.1))
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
         flex: 1
         text-align: center
