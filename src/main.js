499848e96315e1511efc72238fcfee682e09db87
wangxiang <261676607@qq.com>
2017-09-07 23:50:01 +0800

    修复相对路径问题

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
-
 import goods from './components/goods/goods';
 import ratings from './components/ratings/ratings';
 import seller from './components/seller/seller';
-
 
 import 'common/stylus/index.styl';
 
